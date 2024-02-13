85c8ff3e719a8ee81e728dcda14b2053b6d23135
Leopoldo <122998221+poldogs@users.noreply.github.com>
2024-02-13 20:26:20 +0100

    commit

---
+++ 03-counter-app-vite/src/HelloWorldApp.jsx
@@ -0,0 +1,6 @@
+
+export const HelloWorldApp = () => {
+  return (
+    <div>HelloWorldApp</div>
+  )
+}
---
+++ 03-counter-app-vite/src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom';
+import { HelloWorldApp } from './HelloWorldApp';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+        <HelloWorldApp />
+    </React.StrictMode>
+);
