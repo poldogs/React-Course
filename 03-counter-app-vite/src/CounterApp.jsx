cc60306e03ebe7ff00dd2044887acc1475ff65d7
Leopoldo <122998221+poldogs@users.noreply.github.com>
2024-02-19 12:25:47 +0100

    commit

--- 03-counter-app-vite/src/CounterApp.jsx
+++ 03-counter-app-vite/src/CounterApp.jsx
@@ -17,10 +17,10 @@ export const CounterApp = ({ value }) => {
     return (
         <>
             <h1>CounterApp</h1>
-            <h2> { counter } </h2>
+            <h2 id='counter'> { counter } </h2>
 
-            <button onClick={ handleAdd }> +1 </button>
-            <button onClick={ handleSubstract }> -1 </button>
+            <button onClick={ handleAdd } id='btn-add'> +1 </button>
+            <button onClick={ handleSubstract } id='btn-substact'> -1 </button>
             <button aria-label="btn-reset" onClick={ handleReset }> Reset </button>
         </>
     );
--- 03-counter-app-vite/tests/CounterApp.test.jsx
+++ 03-counter-app-vite/tests/CounterApp.test.jsx
@@ -6,18 +6,16 @@ describe('Pruebas en el <CounterApp />', () => {
 
     const initialValue = 10;
     
-    test('debe de hacer match con el snapshot', () => {
-        const { container } = render(<CounterApp value={ initialValue } />);
-        expect( container ).toMatchSnapshot();
-    });
+    test('de hacer match con el snapshot', () => { 
+        const wrapper = render( <CounterApp value={ initialValue } /> );
+        expect( wrapper ).toMatchSnapshot();
+     })
 
-    test('debe de mostrar el valor inicial de 100 <CounterApp value={100}>', () => {
-        
+    test('el valor inicial debe ser 100', () => { 
         render( <CounterApp value={ 100 } /> );
-        expect( screen.getByText(100) ).toBeTruthy();
-        // expect( screen.getByRole('heading',{ level: 2}).innerHTML ).toContain('100')
-
-    });
+        expect( screen.getByText( 100 ) ).toBeTruthy();
+        //expect( screen.getByRole('heading') ).toHaveTextContent('100');
+     })
 
     test('debe de incrementar con el botón +1', () => {
         
@@ -41,12 +39,32 @@ describe('Pruebas en el <CounterApp />', () => {
         fireEvent.click( screen.getByText('+1') );
         fireEvent.click( screen.getByText('+1') );
         fireEvent.click( screen.getByText('+1') );
+
+        expect( screen.getByText('358') ).toBeTruthy();
+
         // fireEvent.click( screen.getByText('Reset') );
         fireEvent.click(screen.getByRole('button', { name: 'btn-reset' }));
 
         expect( screen.getByText( 355 ) ).toBeTruthy();
 
     });
+
+    test('deben de funcionar todos los botones', () => { 
+        render( <CounterApp value={ initialValue } /> );
+        fireEvent.click( screen.getByText('+1') );
+        fireEvent.click( screen.getByText('+1') );
+        fireEvent.click( screen.getByText('+1') );
+
+        expect( screen.getByText('13') ).toBeTruthy();
+
+        fireEvent.click( screen.getByText('-1') );
+
+        expect( screen.getByText('12') ).toBeTruthy();
+
+        fireEvent.click( screen.getByText('Reset') );
+
+        expect( screen.getByText( initialValue ) ).toBeTruthy();
+     })
 
 
 
